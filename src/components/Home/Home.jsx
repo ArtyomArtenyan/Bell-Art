a094563212eec51846f9e1d708e877baa8bfdd78
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2023-09-12 23:49:30 +0400

    finish advantages in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,10 @@ import slidePhoto2 from "../../components/Pictures/heroSection/slide-photo-2.png
 import slidePhoto3 from "../../components/Pictures/heroSection/slide-photo-3.png";
 import SlideLeft from "../../components/Pictures/heroSection/slide-left.svg";
 import SlideRight from "../../components/Pictures/heroSection/slide-right.svg";
+import AdvantagesBgPhoto1 from "../../components/Pictures/Advantages/bg-photo.png";
+import AdvantagesBgPhoto2 from "../../components/Pictures/Advantages/bg-photo2.png";
+import AdvantagesBgPhoto3 from "../../components/Pictures/Advantages/bg-photo3.png";
+
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
@@ -26,11 +30,9 @@ const Home = () => {
   const handleNextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide === max - 1 ? 0 : prevSlide + 1));
   };
-
   return (
     <div className="Home gap-[120px] inline-flex flex-col items-center">
       {/* HeroSection */}
-
       <div className="HeroSection relative w-[1920px] h-[807px] overflow-hidden">
         <div
           className="slide-photo-container absolute duration-300 flex "
@@ -76,6 +78,48 @@ const Home = () => {
         </div>
         <div className="AboutUsText w-[1341px] h-[360px]">
           <p className="text-[20px] font-light leading-9">{t("aboutUsText")}</p>
+        </div>
+      </div>
+      {/* Advantages */}
+      <div className="Advantages w-[1520px] h-[681px] shadow-custom relative">
+        <div className="AdvantagesUs text-[#202020] w-[625px] h-[59px] absolute top-[120px] left-20">
+          <p className="text-[48px] font-normal leading-[58.51px]">
+            {t("advantagesUs")}
+          </p>
+        </div>
+        <div className="AdvantagesLayout w-[1256px] h-[282px] absolute left-[88px] top-[259px] gap-12">
+          <div className="Layout inline-flex items-start gap-12 z-0">
+            <div className="LayoutBox1 w-[400px] h-[282px] shrink-0 relative">
+              <img src={AdvantagesBgPhoto1} alt="Advantages Image 1" />
+              <div className="absolute inset-0 bg-[#202020] opacity-[0.8]"></div>
+              <span className="absolute top-10 left-[178px] w-12 h-[96px] font-light text-[80px] leading-[97.52px] items-center text-[#fff]">
+                1
+              </span>
+              <p className="absolute top-[162px] left-10 w-[320px] h-20 text-center text-[20px] font-light leading-[24.38px] text-[#fff]">
+                {t("advantagesText1")}
+              </p>
+            </div>
+            <div className="LayoutBox2 w-[400px] h-[282px] shrink-0 relative">
+              <img src={AdvantagesBgPhoto2} alt="Advantages Image 2" />
+              <div className="absolute inset-0 bg-[#202020] opacity-[0.8]"></div>
+              <span className="absolute top-10 left-[178px] w-12 h-[96px] font-light text-[80px] leading-[97.52px] items-center text-[#fff]">
+                2
+              </span>
+              <p className="absolute top-[162px] left-10 w-[320px] h-20 text-center text-[20px] font-light leading-[24.38px] text-[#fff]">
+                {t("advantagesText2")}
+              </p>
+            </div>
+            <div className="LayoutBox3 w-[400px]  h-[282px] shrink-0 relative">
+              <img src={AdvantagesBgPhoto3} alt="Advantages Image 3" />
+              <div className="absolute inset-0 bg-[#202020] opacity-[0.8]"></div>
+              <span className="absolute top-10 left-[178px] w-12 h-[96px] font-light text-[80px] leading-[97.52px] items-center text-[#fff]">
+                3
+              </span>
+              <p className="absolute top-[162px] left-10 w-[320px] h-20 text-center text-[20px] font-light leading-[24.38px] text-[#fff]">
+                {t("advantagesText3")}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
